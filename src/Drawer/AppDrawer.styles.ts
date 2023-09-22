285cb6426793abd837660cc68caf3b54b4cf6f2d
Ben Beck <ben.beck.764@gmail.com>
2023-09-22 11:01:35 -0500

    Add Drawer

---
+++ src/Drawer/AppDrawer.styles.ts
@@ -0,0 +1,64 @@
+import { styled, Button, Box, Drawer, Typography } from "@mui/material/";
+
+export const StyledList = styled("ul")({
+  padding: "6px 0px",
+  listStyleType: "none",
+  "&:focus": { outline: "none" },
+});
+
+export const StyledPanelBox = styled(Box)({
+  padding: "16px 16px",
+  "&:focus": { outline: "none" },
+});
+
+interface AppDrawerDisplayMode {
+  displayAboveHeader?: boolean;
+}
+
+export const StyledDrawer = styled(Drawer, {
+  shouldForwardProp: (prop) => prop !== "displayAboveHeader",
+})<AppDrawerDisplayMode>(({ theme, displayAboveHeader }) => ({
+  ...(displayAboveHeader
+    ? {}
+    : {
+        zIndex: 100,
+        "& .MuiDrawer-paper": {
+          [theme.breakpoints.up("xs")]: {
+            top: theme.custom.headerHeights["xs"],
+          },
+          [theme.breakpoints.up("xl")]: {
+            top: theme.custom.headerHeights["xl"],
+          },
+        },
+      }),
+}));
+
+export const StyledButton = styled(Button)(({ theme }) => ({
+  "&.MuiButton-sizeMedium": {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+}));
+
+export const StyledMenuLabel = styled(Typography)(({ theme }) => ({
+  [theme.breakpoints.up("xs")]: {
+    ...theme.typography.mobileParagraph,
+  },
+  [theme.breakpoints.up("xl")]: {
+    ...theme.typography.paragraph,
+  },
+}));
+
+export const StyledContent = styled(Typography)<{ $selected?: boolean }>(
+  ({ $selected, theme }) => ({
+    ...($selected
+      ? {
+          "&": { ...theme.typography.mobileParagraphBold },
+          color: theme.palette.primary.main,
+        }
+      : {
+          "&": { ...theme.typography.mobileParagraph },
+          color: theme.palette.grey[900],
+        }),
+  })
+);
