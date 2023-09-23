3f099246e5244d0c747f67e02e22f901af82fae1
Ben Beck <ben.beck.764@gmail.com>
2023-09-23 13:38:54 -0500

    v1.0.14 - Ensuring peer dependencies are not bundled

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,35 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig ({
+export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "react-components", 
+      name: "react-components",
       fileName: "index",
     },
     rollupOptions: {
-      external: ["react"],
+      external: [
+        "@emotion/react",
+        "@emotion/styled",
+        "@mui/icons-material",
+        "@mui/material",
+        "@mui/styled-engine-sc",
+        "@mui/x-date-pickers",
+        "moment",
+        "react",
+        "react-dom",
+        "react-dropzone",
+        "react-image-crop",
+        "react-virtuoso",
+        "react-quill",
+      ],
     },
   },
   resolve: {
     alias: {
       "@common": resolve(__dirname, "src/_common/index.ts"),
-      "@utilities": resolve(__dirname, "src/_utilities/index.ts")
-    }
-  }
+      "@utilities": resolve(__dirname, "src/_utilities/index.ts"),
+    },
+  },
 });
