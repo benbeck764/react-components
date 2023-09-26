28cd3076ecdcd67192c471625d1fc64b3937d6dc
Ben Beck <ben.beck.764@gmail.com>
2023-09-25 22:57:40 -0500

    v1.0.23 - Testing stuff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  optimizeDeps: {
+    include: ["@mui/material"],
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
@@ -25,4 +29,5 @@ export default defineConfig({
       "@utilities": resolve(__dirname, "src/_utilities/index.ts"),
     },
   },
+  plugins: [dts()],
 });
