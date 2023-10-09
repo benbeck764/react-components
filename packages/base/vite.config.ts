d10cee28e2394a89ec6932e8708a6c7f11641007
Ben Beck <ben.beck.764@gmail.com>
2023-10-09 15:20:51 -0500

    v.1.3.6 - Testing Vite build again, trying to preserve "use client" directives

--- packages/base/vite.config.ts
+++ packages/base/vite.config.ts
@@ -1,21 +1,27 @@
 import { defineConfig } from "vite";
+import preserveDirectives from "rollup-plugin-preserve-directives";
+import noBundlePlugin from "vite-plugin-no-bundle";
 import dts from "vite-plugin-dts";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 import { resolve } from "path";
 import pkg from "./package.json";
 
 export default defineConfig({
   build: {
     lib: {
+      formats: ["es"],
       entry: resolve(__dirname, "src/index.ts"),
       name: "react-components",
-      fileName: "base",
-      formats: ["es", "umd", "cjs"],
     },
     rollupOptions: {
       external: [...Object.keys(pkg.peerDependencies)],
     },
-    outDir: "dist",
   },
-  plugins: [dts({ tsconfigPath: "./tsconfig.json" })],
+  plugins: [
+    dts({
+      entryRoot: "src",
+      exclude: ["src/_stories/**/*", "src/**/*.stories.tsx"],
+    }),
+    preserveDirectives(),
+    noBundlePlugin(),
+  ],
 });
