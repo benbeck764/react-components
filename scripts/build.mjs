da5f0d75e38fa4570565b39f7772ddb9d7de48ab
Ben Beck <ben.beck.764@gmail.com>
2023-10-19 16:20:24 -0500

    More scripting

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,26 +1,55 @@
-import { spawn } from "child_process";
-import path from "path";
+import { exec } from "child_process";
 import fs from "fs";
 
-// Read the existing tsconfig.json file
-const tsconfig = JSON.parse(fs.readFileSync("tsconfig.esm.json", "utf8"));
+function modifyTSConfig(moduleType, revert) {
+  // Read the existing tsconfig.json file
+  const tsconfig = JSON.parse(
+    fs.readFileSync(`tsconfig.${moduleType}.json`, "utf8")
+  );
 
-// Modify the tsconfig.json as needed
-tsconfig.compilerOptions.outDir = ".";
-tsconfig.compilerOptions.declarationDir = ".";
-
-// Write the modified tsconfig.json back to the file
-fs.writeFileSync("tsconfig.esm.json", JSON.stringify(tsconfig, null, 2));
-
-// Run tsc with the modified configuration
-const tsc = spawn("tsc", ["--project", "tsconfig.esm.json"], {
-  cwd: process.cwd(),
-});
-
-tsc.on("close", (code) => {
-  if (code === 0) {
-    console.log("TypeScript compilation succeeded.");
-  } else {
-    console.error("TypeScript compilation failed.");
+  // Modify the tsconfig.json as needed
+  if (moduleType === "esm") {
+    tsconfig.compilerOptions.outDir = revert ? "dist" : "./";
+    tsconfig.compilerOptions.declarationDir = revert ? "dist" : "./";
+  } else if (moduleType === "common") {
+    tsconfig.compilerOptions.outDir = revert ? "dist/node" : "./node";
   }
-});
+
+  // Write the modified tsconfig.json back to the file
+  fs.writeFileSync(
+    `tsconfig.${moduleType}.json`,
+    JSON.stringify(tsconfig, null, 2)
+  );
+}
+
+function build() {
+  const args = process.argv.slice(2);
+  const moduleType = args[0];
+
+  const validModuleTypes = ["esm", "common"];
+  if (validModuleTypes.indexOf(moduleType) === -1) {
+    throw new TypeError(
+      `Unrecognized module type '${moduleType}'. Did you mean one of "${validModuleTypes.join(
+        '", "'
+      )}"?`
+    );
+  }
+
+  // Temporarily Modify tsconfig.{moduleType}.json file
+  modifyTSConfig(moduleType, false);
+
+  exec(`pnpm build:${moduleType}`, (error, stdout) => {
+    if (error) {
+      console.error(err);
+      console.log(`Build failed :(`);
+    } else {
+      console.log(stdout);
+      console.log(`Build success!`);
+    }
+
+    // Cleanup tsconfig
+    modifyTSConfig(moduleType, true);
+  });
+}
+
+build();
