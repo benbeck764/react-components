a2fc31d79c2ba6f897a4602efb5b4ab5cb5bd91b
Ben Beck <ben.beck.764@gmail.com>
2023-10-30 20:30:49 -0500

    Minor export fix

--- packages/base/src/ToggleButtonGroup/AppToggleButton.tsx
+++ packages/base/src/ToggleButtonGroup/AppToggleButton.tsx
@@ -3,16 +3,14 @@ import { FC, PropsWithChildren } from "react";
 import { SxProps, Theme } from "@mui/material/styles";
 import ToggleButton from "@mui/material/ToggleButton";
 
-type AppToggleButtonProps = {
+export type AppToggleButtonProps = {
   value: string;
   sx?: SxProps<Theme>;
 };
 
-const AppToggleButton: FC<PropsWithChildren<AppToggleButtonProps>> = (
+export const AppToggleButton: FC<PropsWithChildren<AppToggleButtonProps>> = (
   props: PropsWithChildren<AppToggleButtonProps>
 ) => {
   const { children, ...toggleButtonProps } = props;
   return <ToggleButton {...toggleButtonProps}>{children}</ToggleButton>;
 };
-
-export default AppToggleButton;
